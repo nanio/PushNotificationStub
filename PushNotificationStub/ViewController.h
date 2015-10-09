e2ca4d9bedaa957177226efd104c652f6b97ad3c
Thomas Federer <thomas.federer@nanio.com>
2015-10-09 13:51:19 +0200

    initial project commit
    Signed-off-by: Thomas Federer <thomas.federer@nanio.com>

---
+++ PushNotificationStub/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PushNotificationStub
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PushNotificationStubTests/PushNotificationStubTests.m
@@ -0,0 +1,40 @@
+//
+//  PushNotificationStubTests.m
+//  PushNotificationStubTests
+//
+//  Created by Thomas Federer on 05.08.15.
+//  Copyright (c) 2015 nanio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PushNotificationStubTests : XCTestCase
+
+@end
+
+@implementation PushNotificationStubTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
