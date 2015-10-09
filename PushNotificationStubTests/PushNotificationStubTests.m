c365553b5388b6d758ebb15b0b34ec32f8938366
Thomas Federer <thomas.federer@nanio.com>
2015-10-09 13:57:20 +0200

    Removed copyright

--- PushNotificationStubTests/PushNotificationStubTests.m
+++ PushNotificationStubTests/PushNotificationStubTests.m
@@ -2,8 +2,6 @@
 //  PushNotificationStubTests.m
 //  PushNotificationStubTests
 //
-//  Created by Thomas Federer on 05.08.15.
-//  Copyright (c) 2015 nanio. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
